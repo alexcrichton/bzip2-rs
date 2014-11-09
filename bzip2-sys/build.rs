24581d575770cd8a1671e543450399993ab4395d
Alex Crichton <alex@alexcrichton.com>
2014-11-09 00:41:20 -0800

    Update to gcc master

--- bzip2-sys/build.rs
+++ bzip2-sys/build.rs
@@ -1,10 +1,12 @@
 extern crate gcc;
 
+use std::default::Default;
 
 fn main() {
     gcc::compile_library("libbz2.a", &gcc::Config {
         include_directories: vec![Path::new("bzip2-1.0.6")],
         definitions: vec![("BZ_NO_STDIO".to_string(), None)],
+        .. Default::default()
     }, &[
         "bzip2-1.0.6/blocksort.c",
         "bzip2-1.0.6/huffman.c",
