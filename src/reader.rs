00b7f69919b0d38f74eea1179f52faa4997b46b8
Alex Crichton <alex@alexcrichton.com>
2015-03-02 16:15:21 -0800

    Don't try to use read_to_end when reading
    Each call to `read` should delegate to as few underlying calls to `read` as
    possible.

--- src/reader.rs
+++ src/reader.rs
@@ -2,26 +2,25 @@
 
 use std::io::prelude::*;
 use std::io;
+use std::iter::repeat;
+use libc::c_int;
 
 use ffi;
 use raw::{Stream, Action};
 
 /// A compression stream which wraps an uncompressed stream of data. Compressed
 /// data will be read from the stream.
-pub struct BzCompressor<R> {
-    stream: Stream,
-    r: R,
-    buf: Vec<u8>,
-    pos: usize,
-    done: bool,
-}
+pub struct BzCompressor<R>(Inner<R>);
 
 /// A decompression stream which wraps a compressed stream of data. Decompressed
 /// data will be read from the stream.
-pub struct BzDecompressor<R> {
+pub struct BzDecompressor<R>(Inner<R>);
+
+struct Inner<R> {
     stream: Stream,
     r: R,
     buf: Vec<u8>,
+    cap: usize,
     pos: usize,
     done: bool,
 }
@@ -30,50 +29,27 @@ impl<R: Read> BzCompressor<R> {
     /// Create a new compression stream which will compress at the given level
     /// to read compress output to the give output stream.
     pub fn new(r: R, level: ::Compress) -> BzCompressor<R> {
-        BzCompressor {
+        BzCompressor(Inner {
             stream: Stream::new_compress(level, 30),
             r: r,
-            buf: Vec::with_capacity(128 * 1024),
+            buf: repeat(0).take(32 * 1024).collect(),
+            cap: 0,
             pos: 0,
             done: false,
-        }
+        })
     }
 
     /// Unwrap the underlying writer, finishing the compression stream.
-    pub fn into_inner(self) -> R { self.r }
+    pub fn into_inner(self) -> R { self.0.r }
 }
 
 impl<R: Read> Read for BzCompressor<R> {
     fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        if self.done { return Ok(0) }
-
-        let mut eof = false;
-        if self.pos == self.buf.len() {
-            self.buf.truncate(0);
-            try!(self.r.by_ref().take(self.buf.capacity() as u64)
-                       .read_to_end(&mut self.buf));
-            eof = self.buf.len() < self.buf.capacity();
-            self.pos = 0;
-        }
-
-        let before_in = self.stream.total_in();
-        let before_out = self.stream.total_out();
-        let action = if eof {Action::Finish} else {Action::Run};
-        let rc = self.stream.compress(&self.buf[self.pos..], buf, action);
-        self.pos += (self.stream.total_in() - before_in) as usize;
-        let read = (self.stream.total_out() - before_out) as usize;
-
-        match rc {
-            ffi::BZ_STREAM_END if read > 0 => self.done = true,
-            ffi::BZ_OUTBUFF_FULL |
-            ffi::BZ_STREAM_END => {}
-            n if n >= 0 => {}
-
-            _ => return Err(io::Error::new(io::ErrorKind::InvalidInput,
-                                           "invalid input", None)),
-        }
-
-        Ok(read)
+        println!("compress");
+        self.0.read(|stream, input, eof| {
+            let action = if eof {Action::Finish} else {Action::Run};
+            stream.compress(input, buf, action)
+        })
     }
 }
 
@@ -81,47 +57,61 @@ impl<R: Read> BzDecompressor<R> {
     /// Create a new compression stream which will compress at the given level
     /// to read compress output to the give output stream.
     pub fn new(r: R) -> BzDecompressor<R> {
-        BzDecompressor {
+        BzDecompressor(Inner {
             stream: Stream::new_decompress(false),
             r: r,
-            buf: Vec::with_capacity(128 * 1024),
+            buf: repeat(0).take(32 * 1024).collect::<Vec<_>>(),
+            cap: 0,
             done: false,
             pos: 0,
-        }
+        })
     }
 
     /// Unwrap the underlying writer, finishing the compression stream.
-    pub fn into_inner(self) -> R { self.r }
+    pub fn into_inner(self) -> R { self.0.r }
 }
 
 impl<R: Read> Read for BzDecompressor<R> {
     fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        println!("decompress");
+        self.0.read(|stream, input, _eof| {
+            stream.decompress(input, buf)
+        })
+    }
+}
+
+impl<R: Read> Inner<R> {
+    fn read<F>(&mut self, mut f: F) -> io::Result<usize>
+        where F: FnMut(&mut Stream, &[u8], bool) -> c_int
+    {
         if self.done { return Ok(0) }
 
-        if self.pos == self.buf.len() {
-            self.buf.truncate(0);
-            try!(self.r.by_ref().take(self.buf.capacity() as u64)
-                       .read_to_end(&mut self.buf));
-            self.pos = 0;
+        loop {
+            let mut eof = false;
+            if self.pos == self.cap {
+                self.cap = try!(self.r.read(&mut self.buf));
+                println!("read: {}", self.cap);
+                self.pos = 0;
+                eof = self.cap == 0;
+            }
+            let before_in = self.stream.total_in();
+            let before_out = self.stream.total_out();
+            let rc = f(&mut self.stream, &self.buf[self.pos..self.cap], eof);
+            self.pos += (self.stream.total_in() - before_in) as usize;
+            let read = (self.stream.total_out() - before_out) as usize;
+
+            match rc {
+                ffi::BZ_STREAM_END if read > 0 => self.done = true,
+                ffi::BZ_OUTBUFF_FULL |
+                ffi::BZ_STREAM_END => {}
+                n if n >= 0 => {}
+
+                _ => return Err(io::Error::new(io::ErrorKind::InvalidInput,
+                                               "invalid input", None)),
+            }
+            if read == 0 && !eof { continue }
+            return Ok(read)
         }
-
-        let before_in = self.stream.total_in();
-        let before_out = self.stream.total_out();
-        let rc = self.stream.decompress(&self.buf[self.pos..], buf);
-        self.pos += (self.stream.total_in() - before_in) as usize;
-        let read = (self.stream.total_out() - before_out) as usize;
-
-        match rc {
-            ffi::BZ_STREAM_END if read > 0 => self.done = true,
-            ffi::BZ_OUTBUFF_FULL |
-            ffi::BZ_STREAM_END => {}
-            n if n >= 0 => {}
-
-            _ => return Err(io::Error::new(io::ErrorKind::InvalidInput,
-                                           "invalid input", None)),
-        }
-
-        Ok(read)
     }
 }
 
