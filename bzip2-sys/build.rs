c76698ea88f0044892042105c9e7389509b43977
Alex Crichton <alex@alexcrichton.com>
2018-11-27 10:15:00 -0800

    Install `libbz2.a` in `lib` folders
    Closes #36

--- bzip2-sys/build.rs
+++ bzip2-sys/build.rs
@@ -12,6 +12,8 @@ fn main() {
         cfg.define("BZ_EXPORT", None);
     }
 
+    let dst = PathBuf::from(env::var_os("OUT_DIR").unwrap());
+
     cfg.include("bzip2-1.0.6")
        .define("_FILE_OFFSET_BITS", Some("64"))
        .define("BZ_NO_STDIO", None)
@@ -22,12 +24,13 @@ fn main() {
        .file("bzip2-1.0.6/compress.c")
        .file("bzip2-1.0.6/decompress.c")
        .file("bzip2-1.0.6/bzlib.c")
+       .out_dir(dst.join("lib"))
        .compile("libbz2.a");
 
     let src = env::current_dir().unwrap().join("bzip2-1.0.6");
-    let dst = PathBuf::from(env::var_os("OUT_DIR").unwrap());
     let include = dst.join("include");
     fs::create_dir_all(&include).unwrap();
     fs::copy(src.join("bzlib.h"), dst.join("include/bzlib.h")).unwrap();
     println!("cargo:root={}", dst.display());
+    println!("cargo:include={}", dst.join("include").display());
 }
