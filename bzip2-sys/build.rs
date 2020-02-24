80fb21da9f189bf5af180c277ebc0af5f0c34a80
Alex Crichton <alex@alexcrichton.com>
2020-02-24 08:03:25 -0800

    Update bundled bzip2 to 1.0.8
    Closes #55

--- bzip2-sys/build.rs
+++ bzip2-sys/build.rs
@@ -14,20 +14,20 @@ fn main() {
 
     let dst = PathBuf::from(env::var_os("OUT_DIR").unwrap());
 
-    cfg.include("bzip2-1.0.6")
+    cfg.include("bzip2-1.0.8")
         .define("_FILE_OFFSET_BITS", Some("64"))
         .define("BZ_NO_STDIO", None)
-        .file("bzip2-1.0.6/blocksort.c")
-        .file("bzip2-1.0.6/huffman.c")
-        .file("bzip2-1.0.6/crctable.c")
-        .file("bzip2-1.0.6/randtable.c")
-        .file("bzip2-1.0.6/compress.c")
-        .file("bzip2-1.0.6/decompress.c")
-        .file("bzip2-1.0.6/bzlib.c")
+        .file("bzip2-1.0.8/blocksort.c")
+        .file("bzip2-1.0.8/huffman.c")
+        .file("bzip2-1.0.8/crctable.c")
+        .file("bzip2-1.0.8/randtable.c")
+        .file("bzip2-1.0.8/compress.c")
+        .file("bzip2-1.0.8/decompress.c")
+        .file("bzip2-1.0.8/bzlib.c")
         .out_dir(dst.join("lib"))
         .compile("libbz2.a");
 
-    let src = env::current_dir().unwrap().join("bzip2-1.0.6");
+    let src = env::current_dir().unwrap().join("bzip2-1.0.8");
     let include = dst.join("include");
     fs::create_dir_all(&include).unwrap();
     fs::copy(src.join("bzlib.h"), dst.join("include/bzlib.h")).unwrap();
